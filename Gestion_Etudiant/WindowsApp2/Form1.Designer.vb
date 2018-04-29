18d516ba41a82e986e02bbfbdbf4fbfd9f373995
Rassas Amine <rassasamine@users.noreply.github.com>
2018-04-29 16:37:38 +0200

    Add files via upload

---
+++ Gestion_Etudiant/WindowsApp2/Form1.Designer.vb
@@ -0,0 +1,186 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNom = New System.Windows.Forms.TextBox()
+        Me.txtPrenom = New System.Windows.Forms.TextBox()
+        Me.txtCode = New System.Windows.Forms.TextBox()
+        Me.txtAdresse = New System.Windows.Forms.TextBox()
+        Me.lblCode = New System.Windows.Forms.Label()
+        Me.lblNom = New System.Windows.Forms.Label()
+        Me.lblPrenom = New System.Windows.Forms.Label()
+        Me.lblAdresse = New System.Windows.Forms.Label()
+        Me.btnAjouter = New System.Windows.Forms.Button()
+        Me.btnSupprimer = New System.Windows.Forms.Button()
+        Me.btnModifier = New System.Windows.Forms.Button()
+        Me.btnRechercher = New System.Windows.Forms.Button()
+        Me.cmbNom = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'txtNom
+        '
+        Me.txtNom.Location = New System.Drawing.Point(125, 85)
+        Me.txtNom.Name = "txtNom"
+        Me.txtNom.Size = New System.Drawing.Size(140, 20)
+        Me.txtNom.TabIndex = 0
+        '
+        'txtPrenom
+        '
+        Me.txtPrenom.Location = New System.Drawing.Point(125, 134)
+        Me.txtPrenom.Name = "txtPrenom"
+        Me.txtPrenom.Size = New System.Drawing.Size(140, 20)
+        Me.txtPrenom.TabIndex = 1
+        '
+        'txtCode
+        '
+        Me.txtCode.Location = New System.Drawing.Point(125, 41)
+        Me.txtCode.Name = "txtCode"
+        Me.txtCode.Size = New System.Drawing.Size(140, 20)
+        Me.txtCode.TabIndex = 3
+        '
+        'txtAdresse
+        '
+        Me.txtAdresse.Location = New System.Drawing.Point(125, 182)
+        Me.txtAdresse.Name = "txtAdresse"
+        Me.txtAdresse.Size = New System.Drawing.Size(140, 20)
+        Me.txtAdresse.TabIndex = 2
+        '
+        'lblCode
+        '
+        Me.lblCode.AutoSize = True
+        Me.lblCode.Location = New System.Drawing.Point(23, 44)
+        Me.lblCode.Name = "lblCode"
+        Me.lblCode.Size = New System.Drawing.Size(74, 13)
+        Me.lblCode.TabIndex = 4
+        Me.lblCode.Text = "&Code Etudiant"
+        '
+        'lblNom
+        '
+        Me.lblNom.AutoSize = True
+        Me.lblNom.Location = New System.Drawing.Point(23, 92)
+        Me.lblNom.Name = "lblNom"
+        Me.lblNom.Size = New System.Drawing.Size(29, 13)
+        Me.lblNom.TabIndex = 5
+        Me.lblNom.Text = "&Nom"
+        '
+        'lblPrenom
+        '
+        Me.lblPrenom.AutoSize = True
+        Me.lblPrenom.Location = New System.Drawing.Point(23, 141)
+        Me.lblPrenom.Name = "lblPrenom"
+        Me.lblPrenom.Size = New System.Drawing.Size(43, 13)
+        Me.lblPrenom.TabIndex = 6
+        Me.lblPrenom.Text = "&Prenom"
+        '
+        'lblAdresse
+        '
+        Me.lblAdresse.AutoSize = True
+        Me.lblAdresse.Location = New System.Drawing.Point(23, 189)
+        Me.lblAdresse.Name = "lblAdresse"
+        Me.lblAdresse.Size = New System.Drawing.Size(45, 13)
+        Me.lblAdresse.TabIndex = 7
+        Me.lblAdresse.Text = "&Adresse"
+        '
+        'btnAjouter
+        '
+        Me.btnAjouter.Location = New System.Drawing.Point(290, 34)
+        Me.btnAjouter.Name = "btnAjouter"
+        Me.btnAjouter.Size = New System.Drawing.Size(75, 23)
+        Me.btnAjouter.TabIndex = 8
+        Me.btnAjouter.Text = "Ajouter"
+        Me.btnAjouter.UseVisualStyleBackColor = True
+        '
+        'btnSupprimer
+        '
+        Me.btnSupprimer.Location = New System.Drawing.Point(290, 82)
+        Me.btnSupprimer.Name = "btnSupprimer"
+        Me.btnSupprimer.Size = New System.Drawing.Size(75, 23)
+        Me.btnSupprimer.TabIndex = 9
+        Me.btnSupprimer.Text = "Supprimer"
+        Me.btnSupprimer.UseVisualStyleBackColor = True
+        '
+        'btnModifier
+        '
+        Me.btnModifier.Location = New System.Drawing.Point(290, 131)
+        Me.btnModifier.Name = "btnModifier"
+        Me.btnModifier.Size = New System.Drawing.Size(75, 23)
+        Me.btnModifier.TabIndex = 10
+        Me.btnModifier.Text = "Modifier"
+        Me.btnModifier.UseVisualStyleBackColor = True
+        '
+        'btnRechercher
+        '
+        Me.btnRechercher.Location = New System.Drawing.Point(290, 179)
+        Me.btnRechercher.Name = "btnRechercher"
+        Me.btnRechercher.Size = New System.Drawing.Size(75, 23)
+        Me.btnRechercher.TabIndex = 11
+        Me.btnRechercher.Text = "Rechercher"
+        Me.btnRechercher.UseVisualStyleBackColor = True
+        '
+        'cmbNom
+        '
+        Me.cmbNom.FormattingEnabled = True
+        Me.cmbNom.Location = New System.Drawing.Point(533, 34)
+        Me.cmbNom.Name = "cmbNom"
+        Me.cmbNom.Size = New System.Drawing.Size(121, 21)
+        Me.cmbNom.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(781, 261)
+        Me.Controls.Add(Me.cmbNom)
+        Me.Controls.Add(Me.btnRechercher)
+        Me.Controls.Add(Me.btnModifier)
+        Me.Controls.Add(Me.btnSupprimer)
+        Me.Controls.Add(Me.btnAjouter)
+        Me.Controls.Add(Me.lblAdresse)
+        Me.Controls.Add(Me.lblPrenom)
+        Me.Controls.Add(Me.lblNom)
+        Me.Controls.Add(Me.lblCode)
+        Me.Controls.Add(Me.txtCode)
+        Me.Controls.Add(Me.txtAdresse)
+        Me.Controls.Add(Me.txtPrenom)
+        Me.Controls.Add(Me.txtNom)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNom As TextBox
+    Friend WithEvents txtPrenom As TextBox
+    Friend WithEvents txtCode As TextBox
+    Friend WithEvents txtAdresse As TextBox
+    Friend WithEvents lblCode As Label
+    Friend WithEvents lblNom As Label
+    Friend WithEvents lblPrenom As Label
+    Friend WithEvents lblAdresse As Label
+    Friend WithEvents btnAjouter As Button
+    Friend WithEvents btnSupprimer As Button
+    Friend WithEvents btnModifier As Button
+    Friend WithEvents btnRechercher As Button
+    Friend WithEvents cmbNom As ComboBox
+End Class
---
+++ Gestion_Etudiant/WindowsApp2/Form1.vb
@@ -0,0 +1,128 @@
+﻿Imports System.Data.OleDb
+
+Public Class Form1
+    Dim connexion As OleDbConnection
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim ConnexionString As String
+        ConnexionString = "Provider = Microsoft.Jet.OLEDB.4.0;Data Source= C:\Users\Emine 'HusSoka' Rass\Documents\Visual Studio 2017\Projects\Gestion_Etudiant\WindowsApp2\Database1.mdb"
+        connexion = New OleDbConnection(ConnexionString)
+        connexion.Open()
+
+        Dim reqAffiche As String
+        reqAffiche = "SELECT prenom FROM personne"
+        Dim cmd As OleDbCommand
+        cmd = New OleDbCommand(reqAffiche, connexion)
+
+        Dim reader As OleDbDataReader = cmd.ExecuteReader()
+        While reader.Read()
+            cmbNom.Items.Add(reader.Item(0))
+        End While
+    End Sub
+
+    Private Sub lblCode_Click(sender As Object, e As EventArgs) Handles lblCode.Click
+
+    End Sub
+
+    Private Sub txtCode_TextChanged(sender As Object, e As EventArgs) Handles txtCode.TextChanged
+
+    End Sub
+
+    Private Sub lblNom_Click(sender As Object, e As EventArgs) Handles lblNom.Click
+
+    End Sub
+
+    Private Sub txtNom_TextChanged(sender As Object, e As EventArgs) Handles txtNom.TextChanged
+
+    End Sub
+
+    Private Sub lblPrenom_Click(sender As Object, e As EventArgs) Handles lblPrenom.Click
+
+    End Sub
+
+    Private Sub txtPrenom_TextChanged(sender As Object, e As EventArgs) Handles txtPrenom.TextChanged
+
+    End Sub
+
+    Private Sub lblAdresse_Click(sender As Object, e As EventArgs) Handles lblAdresse.Click
+
+    End Sub
+
+    Private Sub txtAdresse_TextChanged(sender As Object, e As EventArgs) Handles txtAdresse.TextChanged
+
+    End Sub
+
+    Private Sub btnAjouter_Click(sender As Object, e As EventArgs) Handles btnAjouter.Click
+        If (txtCode.Text <> "" And txtNom.Text <> "" And txtPrenom.Text <> "" And txtAdresse.Text <> "") Then
+            Dim req As String
+            req = "INSERT INTO personne (code,nom,prenom,adresse) VALUES(" & Val(txtCode.Text) & ", '" & txtNom.Text & "','" & txtPrenom.Text & "','" & txtAdresse.Text & "')"
+            Dim cmd As OleDbCommand
+            cmd = New OleDbCommand(req, connexion)
+            cmd.ExecuteNonQuery()
+
+            MsgBox("Ajout avec succès !")
+
+            txtCode.Clear()
+            txtNom.Clear()
+            txtPrenom.Clear()
+            txtAdresse.Clear()
+        Else
+            MsgBox("Veuillez entrer les informations !")
+        End If
+    End Sub
+
+    Private Sub btnSupprimer_Click(sender As Object, e As EventArgs) Handles btnSupprimer.Click
+        If (txtCode.Text <> "" And txtNom.Text <> "" And txtPrenom.Text <> "" And txtAdresse.Text <> "") Then
+            Dim deleteStr As String = "DELETE * FROM personne WHERE personne.code = " & Val(txtCode.Text) & ""
+            Dim deleteCMD As OleDbCommand
+            deleteCMD = New OleDbCommand(deleteStr, connexion)
+            deleteCMD.ExecuteNonQuery()
+
+            MsgBox("Suppression avec succès !")
+
+            txtCode.Clear()
+            txtNom.Clear()
+            txtPrenom.Clear()
+            txtAdresse.Clear()
+        Else
+            MsgBox("Veuillez entrer les informations de l'etudiant à supprimer !")
+        End If
+    End Sub
+
+    Private Sub btnModifier_Click(sender As Object, e As EventArgs) Handles btnModifier.Click
+        If (txtCode.Text <> "" And txtNom.Text <> "" And txtPrenom.Text <> "" And txtAdresse.Text <> "") Then
+            Dim updateStr As String = "UPDATE personne SET nom='" & txtNom.Text & "', prenom='" & txtPrenom.Text & "', adresse='" & txtAdresse.Text & "' WHERE personne.code = " & Val(txtCode.Text) & ""
+            Dim updateCMD As OleDbCommand
+            updateCMD = New OleDbCommand(updateStr, connexion)
+            updateCMD.ExecuteNonQuery()
+            MsgBox("Modification avec succès !")
+            txtCode.Clear()
+            txtNom.Clear()
+            txtPrenom.Clear()
+            txtAdresse.Clear()
+        Else
+            MsgBox("Veuillez entrer les informations de l'etudiant à modifier !")
+        End If
+    End Sub
+
+    Private Sub btnRechercher_Click(sender As Object, e As EventArgs) Handles btnRechercher.Click
+        If (txtCode.Text <> "") Then
+            Dim req As String
+            req = "SELECT * FROM personne WHERE personne.code = " & Val(txtCode.Text) & ""
+            Dim Cmd As OleDbCommand
+            Cmd = New OleDbCommand(req, connexion)
+            Dim Reader As OleDbDataReader = Cmd.ExecuteReader()
+            If Reader.HasRows Then
+                While Reader.Read()
+                    txtCode.Text = (Reader.Item(0)).ToString()
+                    txtNom.Text = (Reader.Item(1)).ToString()
+                    txtPrenom.Text = (Reader.Item(2)).ToString()
+                    txtAdresse.Text = (Reader.Item(3)).ToString()
+                End While
+            Else
+                MsgBox("Etudiant non trouvé ! Veuillez rechercher par code.")
+            End If
+        Else
+            MsgBox("Veuillez entrer le code de l'etudiant à chercher !")
+        End If
+    End Sub
+End Class
